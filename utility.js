0b977c5de4bb5a96e60330c894f69780a5b9470c
whuey79 <wdhuey@gmail.com>
2014-07-16 16:16:40 -0700

    add pickRandomSeg

--- utility.js
+++ utility.js
@@ -1,10 +1,10 @@
 
-function clearQID(qid) { 
 /*
 * Clear the answers of any question
 * @param {String} qid - question id
 * Tested
 */
+function clearQID(qid) { 
 
   if (f(qid).COMPOUND) {  // question with multiple options
     var fcodes = f(qid).domainValues(); //all codes in f(qid) 
@@ -18,13 +18,13 @@ function clearQID(qid) {
   } 
 }
 
-function pipeLoop(qid,iter) { 
 /*
 * edited pipe func for loop piping
 * @param {String} qid - question id
 * @param {String} iter - loop iteration
 * Tested
 */
+function pipeLoop(qid,iter) { 
 
   var pipedArr = new Array(); 
   var d = a(qid,iter).members(); 
@@ -37,13 +37,13 @@ function pipeLoop(qid,iter) {
   return "["+pipedArr+"]"; 
 }
 
-function setPipe(qid,pre) {
 /*
 * sets id of input to pipe style (readOnly & color) - jQuery
 * @param {String} qid - question id
 * @param {String} pre - question precode
 * Untested
 */
+function setPipe(qid,pre) {
 
   var id = '#' + qid;
 
@@ -57,23 +57,24 @@ function setPipe(qid,pre) {
   }
 }
 
-function newPopup(url) {
 /*
 * Sawtooth popup function (JS)
 * @param {String} url - destination url
 * Tested
 */
+function newPopup(url) {
+
     popupWindow = window.open(
       url,'popUpWindow','width=900,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
 }
 
-function checkTargetNumLessThanSourceNum(src) {
 /*
 * Validation for single numeric <= numeric
 * @param {String} src - source question id
 * @throws error message
 * Tested
 */
+function checkTargetNumLessThanSourceNum(src) {
 
   var tar = CurrentForm();
   var max = f(src).toNumber();
@@ -85,13 +86,13 @@ function checkTargetNumLessThanSourceNum(src) {
 }
 
 
-function checkTargetListLessThanSourceNum(src,pre) {
 /*
 * validation for numeric list <= single numeric 
 * @param {String} src - source question id
 * @param { Int | String } pre (Optional) - question precode
 * Untested
 */
+function checkTargetListLessThanSourceNum(src,pre) {
 
   var tar = CurrentForm();
   var dv = f(tar).domainValues();
@@ -111,12 +112,12 @@ function checkTargetListLessThanSourceNum(src,pre) {
   }
 }
 
-function checkTargetListLessThanSourceList(src) {
 /*
 * validation for numeric list <= numeric list (assume same list)
 * @param {String} src - source question id
 * Untested
 */
+function checkTargetListLessThanSourceList(src) {
 
   var tar = CurrentForm();
   var dv = f(tar).domainValues();
@@ -128,6 +129,121 @@ function checkTargetListLessThanSourceList(src) {
     if( f(tar)[ dv[i] ].toNumber() > max ) {
       RaiseError();
       AppendQuestionErrorMessage(LangIDs.en, "Your answer for " + f(tar)[ dv[i] ].label() + " must be less than or equal to " + max + ".");
+    }
+  }
+}
+
+// /*
+// * -----------------------------------------------------------------------------------
+// * pickMultiSegmentQuota rev3
+// *
+// *    Randomly sets totalNumSeen # of answers in a MULTI, with optional priority precodes
+// *    Useful when you need to show X out of Y brands or sections
+// *
+// *    @param {string} [qid="brandShown"]
+// *        This param needs to be the name of a MULTI that the function 
+// *        will set dependent on parameters.  Use this MULTI as the masking criteria on loops or target grids
+// *
+// *    @param {string} [quotaName="brandCounter"]
+// *        Quota required to close sections if needed.  Assume the quota contains only the MULTI pulled in there
+// *
+// *    @param {number} [totalNumSeen=1]
+// *        total number of options that will be selected (at random) with priorities included 
+// *
+// *    @param {array[]} [alwaysAdd = 'none']
+// *        Array of precodes with priority that will always be included in selection if not quota full
+// *
+// *    @param {array[]} [qidSubSet = f(qid).domainValues()]
+// *        Checks subset of precodes contained in qid that comparisons will only be made against like
+// *        a mask fulfilling criteria ie.  gridMask(qid,5,"<").members()
+// *        otherwise defaults to all precodes in MULTI qid ie. f(qid).domainValues()
+// * -----------------------------------------------------------------------------------
+// */
+
+function pickMultiSegmentQuota( options ) {
+  
+  var defaults = {
+      "qid":"brandShown",
+      "quotaName":"brandCounter",
+      "totalNumSeen": 1, 
+      "alwaysAdd": "none"
+  }  
+  
+  var potentialPrecodesArray = [],
+      sectionsAdded = 0, 
+      rand = 0, 
+      index = 0,
+      i = 0; 
+      
+  var qid = options.qid || defaults.qid, 
+      quotaName = options.quotaName || defaults.quotaName, 
+      totalNumSeen = options.totalNumSeen || defaults.totalNumSeen, 
+      alwaysAdd = options.alwaysAdd || defaults.alwaysAdd;
+
+  // sets potential array to a subset if specified or to all options in the MULTI  
+  var dv = options.qidSubset || f(qid).domainValues();
+
+  // initialize all options in MULTI to 0
+  initMulti(qid);
+    
+  // add priority array if quota is open and is found in subset
+  if ( alwaysAdd !== 'none') {
+      for (i = 0; i < alwaysAdd.length; i++) {  
+        if (checkMultiQF(qid,alwaysAdd[i],quotaName) == false ) {
+          index = getArrayIndex(dv, alwaysAdd[i].toString(), 0);
+
+          if ( index > -1 ) {
+            f(qid)[alwaysAdd[i]].set(1);
+            dv.splice(index,1); 
+            sectionsAdded++;
+          }   
+        }
+      }
+  }
+
+  // Quota: Add section to potential pick if section is open
+  for (i=0; i< dv.length; i++) {
+    if (checkMultiQF(qid,dv[i],quotaName) == false ) {
+      potentialPrecodesArray.push( dv[i] );
+    }
+  }
+
+  // pick remaining totalNumSeen out of N potential items if room available
+  for (i = sectionsAdded; i < totalNumSeen; i++) {
+    rand = Math.floor(Math.random() * potentialPrecodesArray.length);
+      if ( potentialPrecodesArray.length > 0 ) { 
+        f(qid)[potentialPrecodesArray.splice(rand,1)].set(1); 
+        sectionsAdded++;
+      }
+  }  
+
+  // handle scenario when priorities > total num seen.  removes at random
+  while (sectionsAdded > totalNumSeen) {
+    dv = f(qid).categories();
+    rand = Math.floor(Math.random() * dv.length);
+    if ( dv.length > 0 ) { 
+      f(qid)[dv.splice(rand,1)].set(0); 
+    }
+    sectionsAdded--;
+  }
+  
+  
+  function checkMultiQF(q,pre,qName) {
+    var isQuotaFull = false;
+    
+    f(q)[pre].set(1);     
+    if ( qf(qName) )    {
+      isQuotaFull = true;
+    }
+    f(q)[pre].set(0);
+    return isQuotaFull;
+  }
+  
+  function initMulti(qid) {
+    var allOptions = f(qid).domainValues();
+  
+    for (i=0; i< allOptions.length; i++) {
+      f(qid)[allOptions[i]].set(0);
     }
   }
 }
